3ed7f719928f95daca582c8401ad0b51aecff477
Bhavyasoni0123 <132189577+Bhavyasoni0123@users.noreply.github.com>
2026-09-03 13:54:46 +0530

    learning lists

---
+++ LIST & TUPLES.py
@@ -0,0 +1,13 @@
+# LISTS IN PYTHON - 
+marks = [90, 80, 70, 60, 50]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[4])
+print(len(marks))
+# STRINGS ARE IMMMUTABLE WHILE LISTS ARE MUTABLE -
+marks[0] = 100
+print(marks)
+# LIST SLICING -
+print(marks[1:4])
+print(marks[::2])
